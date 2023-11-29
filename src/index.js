0da0367cdabe07143f7130b9f6de4306a1defd06
C4LD4R3LL1 <luiscaldarelli@edu.unifil.br>
2023-11-29 12:54:06 -0300

    Commit Projeto

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+const db = require('./db');
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(db);
+app.use(router);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../paginas", "login.html"));
+});
+
+app.get("/menu.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "../paginas", "menu.html"));
+});
+
+app.get("/cadastro_funcionario.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "../paginas", "cadastro_funcionario.html"));
+});
+
+app.get("/cadastro_produtos.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "../paginas", "cadastro_produtos.html"));
+});
+
+app.use(express.static("paginas"));
+
+app.listen(3333, () => {
+    console.log("Servidor pocando")
+});
